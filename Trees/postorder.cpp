c8b07ebb9200727842971c44c3d48a8ad4c9ed6a
suryansh taragi <67006202+suryansh-code@users.noreply.github.com>
2023-02-07 23:44:50 +0530

    Add files via upload
    traversal methods

---
+++ Trees/postorder.cpp
@@ -0,0 +1,12 @@
+//binary tree
+
+void postOrder(BinaryTreeNode<int> *root) {
+	// Write your code here
+	if(root==NULL)
+	{
+		return ;
+	}
+	postOrder(root->left);
+	postOrder(root->right);
+	cout<<root->data<<" ";
+}
